5215cd93dd9a20aca65fad889ecc4add492ff3ee
kevinsaravia <kevinernestosaravia99@gmail.com>
2020-04-26 17:23:13 -0600

    proyecto_final_app_carwash

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,49 @@
+function obtenerSesion(){
+
+    var nombreUsuario = sessionStorage.getItem('nombre');
+    return (nombreUsuario ===null || nombreUsuario === undefined)?window.location='../CarWash/login.html':false;
+    
+}
+
+function cerrarSesion(){
+    alertify.confirm('Alerta', '¿Está seguro de cerrar esta sesión?',function(){
+        
+        sessionStorage.clear();
+        window.location = '../CarWash/login.html';
+        
+    }, function() {
+        alertify.error('Cancelado');
+        
+    });
+}
+
+function obtenerDatosSesion(){
+    var nombreUsuario = sessionStorage.getItem('nombre');
+    var cargoUsuario = sessionStorage.getItem('cargo');
+    var usuarioUsuario = sessionStorage.getItem('usuario');
+    var codUsuario = sessionStorage.getItem('cod');
+    console.log(nombreUsuario,   cargoUsuario,  usuarioUsuario, codUsuario);
+
+    document.getElementById('nombrecargoUsu').innerHTML=''+nombreUsuario+', '+cargoUsuario;
+    
+}
+
+function validarCargo(){
+    var cargoUsuario = sessionStorage.getItem('cargo');
+
+    if (cargoUsuario==='Mecanico' || cargoUsuario==='Lavador'){
+
+        document.getElementById('personal').style.display = 'none';
+        document.getElementById('mantenimiento').style.display = 'none';
+        document.getElementById('estadisticas').style.display = 'none';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', e => {
 
+    obtenerSesion();
+    obtenerDatosSesion();
+    validarCargo();
     function init() {
     
         $("[class*='mostrar']").click(function () {
@@ -13,7 +57,6 @@ document.addEventListener('DOMContentLoaded', e => {
                 $(`#close-${modulo}`).click(function () {
                     
                     $(`#vista-${modulo}`).hide( "puff", "slow" );
-                    $(`#vista-${modulo}`).html("");
     
                 });
     
