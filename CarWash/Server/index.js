c03da35d985c4e2989dd65b4aa8919570e0f67ff
kevinsaravia <kevinernestosaravia99@gmail.com>
2020-05-22 17:43:00 -0600

    actualizado

--- CarWash/Server/index.js
+++ CarWash/Server/index.js
@@ -4,17 +4,18 @@ var Express = require('express'),
     IO = require('socket.io')(Server),
     MongoClient = require('mongodb').MongoClient,
     url = 'mongodb://localhost:27017',
-    dbName = 'ChatCarWash';
+    dbName = 'ChatSRP';
 
 
 IO.on('connection', function (socket) {
    console.log("El cliente con IP: " + socket.handshake.address+ " se ha conectado...");
-
+    console.log(sessionStorage.getItem('nombre'));
+    
 
    MongoClient.connect(url, function (err, client) {
     const db = client.db(dbName);
 
-    db.collection('chat').find({}).toArray(function (err,msg) {
+    db.collection(`'chat'`).find({}).toArray(function (err,msg) {
         socket.emit('messages', msg);
     });
 
